860b9877c8ae2639323f27b5bbc90bc97a858119
Trongvi-huynh <huynhvi12022000@gmail.com>
2026-05-25 00:55:10 +0900

    Create 10-4.js

---
+++ session10-4/10-4.js
@@ -0,0 +1,5 @@
+let firstNumber = Number(prompt('Nhập vào số thứ nhất: '));
+let secondNumber = Number(prompt('Nhập vào số thứ hai: '));
+
+let total = firstNumber + secondNumber;
+alert('Tổng hai số là:' + total);
