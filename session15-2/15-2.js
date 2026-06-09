ef2e41c5a64663d9e32e49c8975d6de82413bc32
Trongvi-huynh <huynhvi12022000@gmail.com>
2026-06-09 10:21:12 +0900

    Create 15-2.js

---
+++ session15-2/15-2.js
@@ -0,0 +1,5 @@
+const array = prompt("Enter array:");
+console.log("Generate array: " + array);
+const daoNguoc = array.split("").reverse().join("");
+console.log("Reverse array: " + daoNguoc);
+alert("Reverse array: " + daoNguoc);
